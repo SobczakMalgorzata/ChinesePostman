4a15ac8f8783a6b1b0194581c90679b6ce6d45de
Małgorzata Sobczak <sobczakmalgorz@gmail.com>
2016-04-07 17:00:41 +0200

    Test Trees A and B

---
+++ ChinskiListonosz.Core.Tests/TestGraphs/Trees/TreeA.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChinskiListonosz.Core;
+
+namespace ChinskiListonosz.Core.Tests.TestGraphs.Trees
+{
+    public class TreeA
+    {
+        public Graph Create()
+        {
+            return new Graph()
+            {
+                Edges = new List<Edge>()
+                {
+                    new Edge(0, 1),
+                    new Edge(0, 2),
+                    new Edge(0, 3),
+                    new Edge(0, 4),
+                    new Edge(2, 5),
+                    new Edge(3, 6),
+                    new Edge(4, 7),
+                    new Edge(7, 8),
+                    new Edge(7, 9),
+                    new Edge(7, 10),
+                    new Edge(10, 11),
+                    new Edge(10, 12),
+                    new Edge(10, 13)
+                }
+            }; 
+        }
+    }
+}
---
+++ ChinskiListonosz.Core.Tests/TestGraphs/Trees/TreeB.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinskiListonosz.Core.Tests.TestGraphs.Trees
+{
+    public class TreeB
+    {
+        public Graph Create()
+        {
+            return new Graph()
+            {
+                Edges = new List<Edge>()
+                {
+                    new Edge(0, 1),
+                    new Edge(0, 2),
+                    new Edge(0, 3),
+                    new Edge(0, 4),
+                    new Edge(0, 5),
+                    new Edge(1, 6),
+                    new Edge(2, 7),
+                    new Edge(3, 8),
+                    new Edge(4, 9),
+                    new Edge(4, 10),
+                    new Edge(5, 11),
+                    new Edge(5, 12),
+                    new Edge(5, 13)
+                }
+            };
+        }
+    }
+}
