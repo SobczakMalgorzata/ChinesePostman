af38bcf50d8a5ad109e5af098afffcd68410cb93
Małgorzata Sobczak <sobczakmalgorz@gmail.com>
2016-04-07 16:59:49 +0200

    Graph representation classes

---
+++ ChinskiListonosz.Core/Edge.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinskiListonosz.Core
+{
+    public class Edge
+    {
+
+        public int U { get; set; }
+        public int V { get; set; }
+        public int W { get; set; }
+
+        public Edge(int u, int v, int w = 1)
+        {
+            U = u;
+            V = v;
+            W = w;
+        }
+
+        public override bool Equals(System.Object obj)
+        {
+            // If parameter is null return false.
+            if (obj == null)
+            {
+                return false;
+            }
+
+            // If parameter cannot be cast to Edge return false.
+            Edge otherEdge = obj as Edge;
+            return this.Equals(otherEdge);
+        }
+
+        public bool Equals(Edge otherEdge)
+        {
+            // If parameter is null return false:
+            if ((object)otherEdge == null)
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return (W == otherEdge.W 
+                    && ((U == otherEdge.U && V == otherEdge.V) || (U == otherEdge.V && V == otherEdge.U)));
+        }
+    }
+}
---
+++ ChinskiListonosz.Core/Graph.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinskiListonosz.Core
+{
+    public class Graph
+    {
+        public List<Edge> Edges { get; set; }
+
+        public Graph() 
+        {
+             Edges = new List<Edge>();
+        }
+
+        public Graph Kruskal()
+        {
+            List<Edge> kruskal = new List<Edge>();
+            List<List<int>> forest = new List<List<int>>();
+            int uIndex = 0;
+            int vIndex = 0;
+            //TODO Randomize Edges
+            var rand = new System.Random();
+            Edges = Edges.OrderBy(edge => rand.Next()).ToList();
+            foreach (var edge in Edges)
+            {
+                var indexes = FindSubTreesIndexes(forest, edge);
+                uIndex = indexes.Key;
+                vIndex = indexes.Value;
+                if (uIndex == -1 && vIndex == -1)
+                {
+                    forest.Add(new List<int> { edge.U, edge.V });
+                    kruskal.Add(edge);
+                }
+                else if (uIndex == -1)
+                {
+                    forest[vIndex].Add(edge.U);
+                    kruskal.Add(edge);
+                }
+                else if (vIndex == -1)
+                {
+                    forest[uIndex].Add(edge.V);
+                    kruskal.Add(edge);
+                }
+                else if (uIndex != vIndex)
+                {
+                    forest[uIndex].AddRange(forest[vIndex]);
+                    forest.RemoveAt(vIndex);
+                    kruskal.Add(edge);
+                }
+            }
+            return new Graph(){Edges = kruskal};
+        }
+
+        private static KeyValuePair<int, int> FindSubTreesIndexes(List<List<int>> forest, Edge edge)
+        {
+            int uIndex = -1;
+            int vIndex = -1;
+            //uIndex = forest.IndexOf(forest.Where(subTree => subTree.Contains(edge.U));
+            //vIndex = forest.IndexOf(forest.Where(subTree => subTree.Contains(edge.V));
+            foreach (List<int> subTreeVertices in forest)
+            {
+                if (subTreeVertices.Contains(edge.U))
+                    uIndex = forest.IndexOf(subTreeVertices);
+
+                if (subTreeVertices.Contains(edge.V))
+                    vIndex = forest.IndexOf(subTreeVertices);
+            }
+            return new KeyValuePair<int, int>(uIndex, vIndex);
+        }
+    }
+}
