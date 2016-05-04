07c3ae433d45c71768c19a45c25b8c68b072d7e0
Jan Grzybowski <JanGrzybowski@outlook.com>
2016-05-05 01:19:38 +0200

    IGraph description.

--- ChinskiListonosz.Core/IGraph.cs
+++ ChinskiListonosz.Core/IGraph.cs
@@ -8,13 +8,36 @@ namespace ChinskiListonosz.Core
 {
     public interface IGraph
     {
+        /// <summary>
+        /// Returns a list of edges from the graph.
+        /// </summary>
         List<Edge> Edges { get; }
+        /// <summary>
+        /// Returns an unordered list of vertices from the graph.
+        /// </summary>
         List<int> Vertices { get; }
+        /// <summary>
+        /// Checks if the graph is connected.
+        /// </summary>
+        /// <returns></returns>
         bool IsConnected();
+        /// <summary>
+        /// Creates a list of degrees of vertices.
+        /// </summary>
+        /// <returns>List of pairs (vertex number, vertex degree).</returns>
         List<Tuple<int, int>> Degrees();
+        /// <summary>
+        /// Returns number of vertices in graph.
+        /// </summary>
         int NumberOfVertices { get; }
+        /// <summary>
+        /// Returns number of edges in graph.
+        /// </summary>
         int NumberOfEdges{ get; }
+        /// <summary>
+        /// Calculates shortest paths between all vertices in graph.
+        /// </summary>
+        /// <returns>List of Paths connecting different vertices.</returns>
         List<Path> Distances();
-        //int ShortestPathFromTo();
     }
 }
