c0e4da5c2c3f3ee04f35a98af343d77c2ac9cdb8
Jan Grzybowski <JanGrzybowski@outlook.com>
2016-05-21 03:20:16 +0200

    Reduce Implementation

--- ChinskiListonosz.Core/Algorithms/Reduce.cs
+++ ChinskiListonosz.Core/Algorithms/Reduce.cs
@@ -8,9 +8,43 @@ namespace ChinskiListonosz.Core.Algorithms
 {
     public static partial class Al
     {
-        public static IGraph Reduce(this IGraph graph)
+        public static IGraph Reduce(this IGraph tree)
         {
-            throw new NotImplementedException();
+            var stays = new Dictionary<Edge, bool>();
+            foreach (var edge in tree.Edges)
+                stays.Add(edge, true);
+
+            var dists = tree.Distances();
+
+            var evenV = tree.Degrees()
+                        .Where(vDeg => vDeg.Item2.IsEven())
+                        .Select(tup => tup.Item1)
+                        .ToList();
+
+            for (int i = 0; i < evenV.Count(); i+=2)
+            {
+                var p = dists.Single(path => path.Connects(evenV[i], evenV[i + 1]));
+                foreach (var edge in p.Edges)
+                {
+                    stays[edge] = !stays[edge];
+                }
+            }
+
+            var selectedEdges = stays
+                                .Where(keyValue => keyValue.Value)
+                                .Select(keyValue => keyValue.Key)
+                                .ToList();
+
+            return new Graph(tree.Vertices, selectedEdges);
+        }
+
+        private static bool IsEven(this int x)
+        {
+            return x % 2 == 0;
+        }
+        private static bool IsOdd(this int x)
+        {
+            return x % 2 == 1;
         }
     }
 }
