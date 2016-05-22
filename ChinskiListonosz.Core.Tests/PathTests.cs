5a7bafe9bc9030acc4824707e7f3f65e09f0f6bf
a.witwicka <witwickaa@student.mini.pw.edu.pl>
2016-05-22 19:47:01 +0200

    VisitedVertices Field and InsertedAtSuitable method for Path

--- ChinskiListonosz.Core.Tests/PathTests.cs
+++ ChinskiListonosz.Core.Tests/PathTests.cs
@@ -36,5 +36,54 @@ namespace ChinskiListonosz.Core.Tests
             Assert.Equal(3, path.Length);
         }
 
+        [Fact]
+        public void InsertAtBeggining()
+        {
+            var e1 = new Edge(1, 2);
+            var e2 = new Edge(3, 2);
+            var e3 = new Edge(3, 4);
+
+            var list = new List<Edge>() { e1, e2, e3 };
+
+            var path = new Path(list);
+
+            var listCycle = new List<Edge>() { e1, e1 };
+            var pathCycle = new Path(listCycle);
+
+            var expectedPath = new Path(new List<Edge>() { e1, e1, e1, e2, e3 });
+            Assert.Equal(expectedPath, path.InsertAtSuitable(pathCycle));
+        }
+
+        [Fact]
+        public void InsertAtEnd()
+        {
+            var e1 = new Edge(1, 2);
+            var e2 = new Edge(3, 2);
+            var e3 = new Edge(3, 4);
+            var list = new List<Edge>() { e1, e2, e3 };
+            var path = new Path(list);
+
+            var listCycle = new List<Edge>() { e3, e3 };
+            var pathCycle = new Path(listCycle);
+
+            var expectedPath = new Path(new List<Edge>() { e1, e2, e3, e3, e3 });
+            Assert.Equal(expectedPath, path.InsertAtSuitable(pathCycle));
+        }
+
+        [Fact]
+        public void InsertInMiddle()
+        {
+            var e1 = new Edge(1, 2);
+            var e2 = new Edge(3, 2);
+            var e3 = new Edge(3, 4);
+            var list = new List<Edge>() { e1, e2, e3 };
+            var path = new Path(list);
+
+            var listCycle = new List<Edge>() { e2, e2 };
+            var pathCycle = new Path(listCycle);
+
+            var expectedPath = new Path(new List<Edge>() { e1, e2, e2, e2, e3 });
+            Assert.Equal(expectedPath, path.InsertAtSuitable(pathCycle));
+        }
     }
 }
